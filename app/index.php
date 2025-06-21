ce9871b0dc7eed74c9bde2876111447ff99e89c4
devops2c <personele@gmail.com>
2025-06-21 14:37:29 +0100

    aad file

---
+++ app/index.php
@@ -0,0 +1,19 @@
+<?php
+// Connexion MySQL via proxy localhost:3306
+$host = '127.0.0.1';
+$port = 3306;
+$dbname = 'appdb';
+$user = 'root'; // on utilise root car IAM auth bypass la pass (proxy)
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=$charset";
+
+try {
+    $pdo = new PDO($dsn, $user, null, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+    echo "Connexion à la base réussie !";
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
+?>
